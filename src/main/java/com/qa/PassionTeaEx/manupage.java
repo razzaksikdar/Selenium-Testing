0480171ea6f30fe7a8fb83bf643f5bb1ea60ccc9
RAZZAK SIKDAR <arsikdar@gmail.com>
2018-09-20 17:15:28 +0100

    first commit3

--- src/main/java/com/qa/PassionTeaEx/manupage.java
+++ src/main/java/com/qa/PassionTeaEx/manupage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.FindBy;
 public class manupage {
 	@FindBy(xpath="//*[@id=\"wsb-nav-00000000-0000-0000-0000-000450914915\"]/ul/li[3]/a")
 	private WebElement menu;
-	
+	//menu click
 	public void menuClick() {
 		menu.click();
 	}
